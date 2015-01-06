da99df0f2e5ef523eacbfb7ab00b539360f7481b
gagapopa <gagapopa@ya.ru>
2015-01-06 16:11:07 +0600

    модель композит и тест на нее

---
+++ SecondTest/CompositeModelTesting.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Создано в SharpDevelop.
+ * Пользователь: Господин
+ * Дата: 06.01.2015
+ * Время: 15:18
+ * 
+ * Для изменения этого шаблона используйте меню "Инструменты | Параметры | Кодирование | Стандартные заголовки".
+ */
+using System;
+using NUnit.Framework;
+using XmlConvertForIstok.Composite;
+
+namespace SecondTest
+{
+	[TestFixture]
+	public class CompositeModelTesting
+	{
+		[Test]
+		public void CreateNode()
+		{
+			var node = new Node();
+			node.tagName = "TestTagName";
+			node.Name	= "testName";
+			node.Type = "testtype";			
+		}
+		
+		[Test]
+		public void CreateProperty()
+		{
+			var prop = new PropertyNode();
+			prop.tagName = "TestTagName";
+			prop.Name	= "testName";
+			prop.Type = "testtype";
+			prop.Text = "TestText";			
+		}
+		[Test]
+		public void AddNode()
+		{
+			var nodeone = new Node();
+			nodeone.tagName = "TestTagNameone";
+			var nodetwo = new Node();
+			nodetwo.tagName = "TestTagNametwo";
+			
+			nodeone.Nodes.Add(nodetwo);
+			
+			Assert.IsNotNull(nodeone.Nodes.Find(n => n.tagName == "TestTagNametwo"));						
+		}	
+	}
+}
---
+++ XmlConvertForIstok/Composite/Node.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Создано в SharpDevelop.
+ * Пользователь: Господин
+ * Дата: 06.01.2015
+ * Время: 15:22
+ * 
+ * Для изменения этого шаблона используйте меню "Инструменты | Параметры | Кодирование | Стандартные заголовки".
+ */
+using System;
+using System.Collections.Generic;
+
+namespace XmlConvertForIstok.Composite
+{
+	/// <summary>
+	/// Класс древовидной структуры.
+	/// </summary>
+	public abstract class AbstractNode
+	{
+		public string tagName {
+			get;
+			set;
+		}
+		
+		public string Name {
+			get;
+			set;
+		}
+
+		public string Type {
+			get;
+			set;
+		}		
+	}	
+	
+	public class Node : AbstractNode
+	{
+		private List<AbstractNode> nodes = new List<AbstractNode>();
+		
+		public List<AbstractNode> Nodes{
+			get{return nodes;}
+			set{nodes = value;}
+		}
+		
+	}
+	public class PropertyNode : AbstractNode
+	{
+		public string Text {
+			get;
+			set;
+		}
+		
+	}
+}
